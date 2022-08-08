555a8d7c8fa351b170fd79cb7cc35144f7788aa7
Zhafri Shafiq <zhafrishafiq@gmail.com>
2022-08-09 00:30:54 +0800

    Defining aspect

---
+++ src/main/java/io/datajek/springaop/movierecommenderaop/aspect/AccessCheckAspect.java
@@ -0,0 +1,19 @@
+package io.datajek.springaop.movierecommenderaop.aspect;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Configuration;
+
+@Aspect
+@Configuration
+public class AccessCheckAspect {
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @Before("execution(* io.datajek.springaop.movierecommenderaop.business.*.*(..))")
+    public void userAccess(JoinPoint joinPoint) {
+        logger.info("Intercepted call before execution: {}", joinPoint);
+    }
+}
