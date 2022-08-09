9c83097cf359aa2ffa7f88ae34eb61687721c667
Zhafri Shafiq <zhafrishafiq@gmail.com>
2022-08-10 01:17:32 +0800

    JoinPoint Configuration file

--- src/main/java/io/datajek/springaop/movierecommenderaop/aspect/AccessCheckAspect.java
+++ src/main/java/io/datajek/springaop/movierecommenderaop/aspect/AccessCheckAspect.java
@@ -12,13 +12,16 @@ import org.springframework.context.annotation.Configuration;
 public class AccessCheckAspect {
     private Logger logger = LoggerFactory.getLogger(this.getClass());
 
+    @Before("io.datajek.springaop.movierecommenderaop.aspect.JoinPointConfig.movieBeanPointcut()") //intercepting bean using @Pointcut
+    //@Before("io.datajek.springaop.movierecommenderaop.aspect.JoinPointConfig.businessLayerPointcut()") //intercepting business layer using @Pointcut
+    //@Before("io.datajek.springaop.movierecommenderaop.aspect.JoinPointConfig.allLayersPointcut()") //intercepting all layers using @Pointcut
     //@Before("execution(* io.datajek.springaop.movierecommenderaop.business.*.*(String,..))") // Intercepting all method calls in a package
     //@Before("execution(* io.datajek.springaop.movierecommenderaop..*.*(..))") // Intercepting all method calls
     //@Before("execution(String io.datajek.springaop.movierecommenderaop..*.*(..))") // Intercepting calls using return type
     //@Before("execution(String io.datajek.springaop.movierecommenderaop..*.*Filtering(..))") // Intercepting calls to a specific method
     //@Before("execution(* io.datajek.springaop.movierecommenderaop..*.*(String,..))") // Intercepting calls with specific method arguments
-    @Before("execution(* io.datajek.springaop.movierecommenderaop..*.*Filtering(..)) || execution(String io.datajek.springaop.movierecommenderaop..*.*(..))")
-    //Combining pointcut expressions#
+    //@Before("execution(* io.datajek.springaop.movierecommenderaop..*.*Filtering(..)) || execution(String io.datajek.springaop.movierecommenderaop..*.*(..))")
+    //Combining pointcut expressions
     public void userAccess(JoinPoint joinPoint) {
         //intercept a call
         logger.info("Intercepted call before execution: {}", joinPoint);
---
+++ src/main/java/io/datajek/springaop/movierecommenderaop/aspect/JoinPointConfig.java
@@ -0,0 +1,21 @@
+package io.datajek.springaop.movierecommenderaop.aspect;
+
+import org.aspectj.lang.annotation.Pointcut;
+
+public class JoinPointConfig {
+
+    @Pointcut("execution(* io.datajek.springaop.movierecommenderaop.data.*.*(String,..))")
+    public void dataLayerPointcut() {}
+
+    @Pointcut("execution(* io.datajek.springaop.movierecommenderaop.business.*.*(String,..))")
+    public void businessLayerPointcut() {}
+
+    //to intercept method calls for both layers:
+    @Pointcut("io.datajek.springaop.movierecommenderaop.aspect.JoinPointConfig.dataLayerPointcut() "
+        + "io.datajek.springaop.movierecommenderaop.aspect.JoinPointConfig.businessLayerPointcut()")
+    public void allLayersPointcut() {}
+
+    //for a particular bean
+    @Pointcut("bean(movie*)")
+    public void movieBeanPointcut() {}
+}
